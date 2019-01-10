c423338af70adf4764ed8f515b493a0d58b51c8c
Paulo Henrique <paulorodrigueslt@gmail.com>
2019-01-09 22:06:53 -0200

    Aula gravação de produtos em arquivo txt

---
+++ Aula01/Projeto01/Controles/ProdutoControle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto01.Entidades;
+using System.IO; //input, output
+
+namespace Projeto01.Controles
+{
+    class ProdutoControle
+    {
+        public void GravarArquivo(Produto produto)
+        {
+            StreamWriter sw = new StreamWriter("c:\\temp\\produtos.txt", true);
+            sw.WriteLine("Código..........:" + produto.Codigo);
+            sw.WriteLine("Nome..........:" + produto.Nome);
+            sw.WriteLine("Preço..........:" + produto.Preco);
+            sw.WriteLine("Quantidade..........:" + produto.Quantidade);
+            sw.WriteLine("----------------------");
+
+            //fecha o arquivo
+            sw.Close();
+        }
+    }
+}
---
+++ Aula01/Projeto01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto01.Controles;
+
+namespace Projeto01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+            ProdutoControle produtoControle new = produtoControle();
+            produtoControle.GravarArquivo(produto);
+
+            Console.WriteLine("\nProduto gravado com sucesso!");
+
+            Console.ReadKey();
+        }
+    }
+}
